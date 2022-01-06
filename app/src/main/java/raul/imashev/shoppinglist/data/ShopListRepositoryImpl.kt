ae9b45f1dde195673b9429d5cad19f993a197c98
Raul <raulimashev@yandex.ru>
2022-01-06 13:10:26 +0300

    add shop list adapter

--- app/src/main/java/raul/imashev/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/raul/imashev/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -9,12 +9,13 @@ import java.lang.RuntimeException
 object ShopListRepositoryImpl : ShopListRepository {
 
     private val shopListLD = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList =
+        sortedSetOf<ShopItem>({ o1, o2 -> o1.id.compareTo(o2.id) }) //сравнение элементов списка по id
     private var autoIncrementId = 0
 
     init {
         for (i in 0 until 10) {
-            val a = ShopItem("name $i",1,true)
+            val a = ShopItem("name $i", 1, true)
             addShopListItem(a)
         }
     }
---
+++ app/src/main/java/raul/imashev/shoppinglist/presentation/ShopListAdapter.kt
@@ -0,0 +1,41 @@
+package raul.imashev.shoppinglist.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import raul.imashev.shoppinglist.R
+import raul.imashev.shoppinglist.domain.ShopItem
+
+class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_shop_enabled,
+            parent,
+            false
+        )
+        return ShopItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        val shopItem = shopList[position]
+        holder.tvName.text = shopItem.name
+        holder.tvCount.text = shopItem.count.toString()
+    }
+
+    override fun getItemCount(): Int = shopList.size
+
+    class ShopItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    }
+
+}
