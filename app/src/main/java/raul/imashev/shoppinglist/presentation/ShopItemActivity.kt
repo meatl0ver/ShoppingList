0494d5f98d9dea8086305b434100343c6a8d32fd
Raul <raulimashev@yandex.ru>
2022-01-13 12:51:08 +0300

    add second screen layout

---
+++ app/src/main/java/raul/imashev/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package raul.imashev.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import raul.imashev.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
